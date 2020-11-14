a0dafcd7e0574812655466dea5a3e31e84528ea8
abrardewa <58102178+abrardewa@users.noreply.github.com>
2020-11-14 23:20:51 +0700

    Add files via upload

---
+++ latihan1.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>latihan1</title>
+  </head>
+  <body>
+    <?php
+      $a=20;
+      $b=5;
+        echo "bilangan 1 = ".$a;
+        echo "<br>";
+        echo "bilangan 2 = ".$b;
+        echo "<br>";
+
+        echo "Berikut merupakan hasil dari setiap operasi";
+        echo "<br>";
+        echo "<br>";
+        $hasil1=$a+$b;
+      echo "PENJUMLAHAN <br>";
+      echo "Operator : +";
+      echo "<br>";
+      echo "Hasil : ".$hasil1;
+      echo "<br>";
+      echo "<br>";
+
+
+      $hasil2=$a-$b;
+      echo "PENGURANGAN";
+      echo "<br>";
+      echo "Operator : -";
+      echo "<br>";
+      echo "Hasil : ".$hasil2;
+      echo "<br>";
+      echo "<br>";
+
+      $hasil3=$a*$b;
+      echo "PERKALIAN";
+      echo "<br>";
+      echo "Operator : *";
+      echo "<br>";
+      echo "Hasil : ".$hasil3;
+      echo "<br>";
+      echo "<br>";
+
+      $hasil4=$a/$b;
+      echo "PEMBAGIAN";
+      echo "<br>";
+      echo "Operator : /";
+      echo "<br>";
+      echo "Hasil : ".$hasil4;
+      echo "<br>";
+      echo "<br>";
+
+      $hasil5=$a%$b;
+      echo "MODULUS";
+      echo "<br>";
+      echo "Operator : % ";
+      echo "<br>";
+      echo "Hasil : ".$hasil5;
+     ?>
+
+  </body>
+</html>
